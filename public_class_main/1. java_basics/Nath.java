b528b35d9f4c13e279a0eea8b87c832d24a63c4c
stealthiq <79656108+StealthIQ@users.noreply.github.com>
2024-05-03 19:05:49 +0530

    Learned math module

---
+++ public_class_main/1. java_basics/Nath.java
@@ -0,0 +1,22 @@
+public class Nath{
+  public static void main(String[] args){
+    // Max number
+    // Min number
+    int x = Math.max(5, 10);
+    int y = Math.min(5, 10);
+    // Random number from 1 to 100
+    System.out.println((int) (Math.random() *100));
+    // Random number from 0 - 1
+    System.out.println(Math.random());
+    // Absolute = gives positive value
+    System.out.println(Math.abs(-5.4));
+    // Square root
+    System.out.println(Math.sqrt(8));
+
+    System.out.println(x+y);
+
+    System.out.println((x - y)*(y + x));
+  }
+}
+
+// Make sure not to use class names as file names 
